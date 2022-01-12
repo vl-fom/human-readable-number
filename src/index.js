d304307afd1a3b795e4ad081d457bd58ca074521
Vladislav Fomenko <79394177+vl-fom@users.noreply.github.com>
2022-01-12 04:50:46 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function toReadable (number) {
-  
+  let numbersTo20 = ['zero', 'one', 'two', 'three', 'four', 'five, 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fivteen', 'sixteen', 'seventeen', 'eightteen', 'nineteen', 'twelve'];
+  let decimal = ['zero', 'ten', 'twenty', 'trirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety', 'hundred', 'thousand'];        
+  if (number <= 20) {
+      return numbersTo20[number];
 }
