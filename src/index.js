26f282ced415982072082919717e535001606355
Vladislav Fomenko <79394177+vl-fom@users.noreply.github.com>
2022-01-12 05:04:13 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@ module.exports = function toReadable (number) {
   let decimal = ['zero', 'ten', 'twenty', 'trirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety', 'hundred', 'thousand'];        
   if (number <= 20) {
       return numbersTo20[number];
+  } else if (number < 100) {
+      return (decimal[Math.floor(number / 10)] + ' ' + numbersTo20[number % 10]);
   }
 }
