8329930643b3ab1d4a2ce0e02dca7aad67ac22c4
Vladislav Fomenko <79394177+vl-fom@users.noreply.github.com>
2022-01-19 02:27:22 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 module.exports = function toReadable (number) {
-  let numbersTo20 = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 'twenty'];
-  let decimal = ['zero', 'ten', 'twenty', 'trirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety', 'hundred', 'thousand'];
+  const numbersTo20 = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 'twenty'];
+  const decimal = ['zero', 'ten', 'twenty', 'trirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
   if (number <= 20) {
         return numbersTo20[number];
   } else if ((number < 100) && (number % 10 == 0)) {
